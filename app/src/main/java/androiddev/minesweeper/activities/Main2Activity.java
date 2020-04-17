c114b9989975488c267236e061d7734cb5aa7109
vladb <vlad.bell92@gmail.com>
2020-04-17 12:55:27 +0300

    .

---
+++ app/src/main/java/androiddev/minesweeper/activities/Main2Activity.java
@@ -0,0 +1,15 @@
+package androiddev.minesweeper.activities;
+
+import androiddev.minesweeper.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
